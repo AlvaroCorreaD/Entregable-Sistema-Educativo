c9cfe9144665b25385f7a13acfd39a083bd239b0
irinamarzioni <imarzioni@devartis.com>
2022-10-02 20:50:52 -0300

    correccion

--- index.js
+++ index.js
@@ -7,40 +7,51 @@ var Escuela = /** @class */ (function () {
     Escuela.prototype.getNombreEscuela = function () {
         return this.nombreEscuela;
     };
+    Escuela.prototype.contratarProfesor = function (profesor) {
+        this.listadoProfesores.push(profesor);
+    };
+    Escuela.prototype.despedirProfesor = function (profesor) {
+        for (var index = 0; index < this.listadoProfesores.length; index++) {
+            if (this.listadoProfesores[index].getNombreProfesor() === profesor.getNombreProfesor()) {
+                this.listadoProfesores.splice(index, 1);
+            }
+        }
+    };
+    Escuela.prototype.matricularAlumno = function (alumno) {
+        this.listadoAlumnos.push(alumno);
+    };
+    Escuela.prototype.expulsarAlumno = function (alumno) {
+        for (var index = 0; index < this.listadoAlumnos.length; index++) {
+            if (this.listadoAlumnos[index].getNombreAlumno() === alumno.getNombreProfesor()) {
+                this.listadoAlumnos.splice(index, 1);
+            }
+        }
+    };
     return Escuela;
 }());
-var Profesores = /** @class */ (function () {
-    function Profesores(paramNombreProfesor, paramProfListYN, paramListAlum) {
+var Profesor = /** @class */ (function () {
+    function Profesor(paramNombreProfesor, paramProfListYN, paramListAlum) {
         this.nombreProfesor = paramNombreProfesor;
         this.profesorTieneListado = paramProfListYN;
         this.listadoAlumnos = paramListAlum;
     }
-    Profesores.prototype.getNombreProfesor = function () {
+    Profesor.prototype.getNombreProfesor = function () {
         return this.nombreProfesor;
     };
-    Profesores.prototype.contratarDespedirProfesor = function () {
-        if (this.profesorTieneListado === true) {
-            return "Contratado.";
-        }
-        else {
-            return "Despedido por no tener listado de alumnos.";
-        }
-    };
-    return Profesores;
+    return Profesor;
 }());
-var Alumnos = /** @class */ (function () {
-    function Alumnos(paramNombreAlumno, paramNotaExamen, paramInformNota) {
+var Alumno = /** @class */ (function () {
+    function Alumno(paramNombreAlumno, paramNotaExamen, paramInformNota) {
         this.nombreAlumno = paramNombreAlumno;
         this.notaExamen = paramNotaExamen;
-        this.informarNota = paramInformNota;
     }
-    Alumnos.prototype.getNombreAlumno = function () {
+    Alumno.prototype.getNombreAlumno = function () {
         return this.nombreAlumno;
     };
-    Alumnos.prototype.getNotaExamen = function () {
+    Alumno.prototype.getNotaExamen = function () {
         return this.notaExamen;
     };
-    Alumnos.prototype.getAprobadoDesaprobado = function () {
+    Alumno.prototype.getAprobadoDesaprobado = function () {
         if (this.notaExamen >= 7) {
             return "Aprobado";
         }
@@ -48,34 +59,23 @@ var Alumnos = /** @class */ (function () {
             return "Desaprobado, a recuperatorio";
         }
     };
-    Alumnos.prototype.matricularAlumno = function () {
-        while (this.informarNota === true && this.notaExamen >= 7) {
-            return "Matriculado.";
-        }
-        if (this.informarNota === false && this.notaExamen >= 7) {
-            return "y explusado por no informar su nota.";
-        }
-        else {
-            return "y debe informar su nota en tiempo y forma.";
-        }
-    };
-    return Alumnos;
+    return Alumno;
 }());
-var alumno1 = new Alumnos("Pedro", 9, true);
-var alumno2 = new Alumnos("Juan", 7, false);
-var alumno3 = new Alumnos("Lucas", 3, true);
+var alumno1 = new Alumno("Pedro", 9, true);
+var alumno2 = new Alumno("Juan", 7, false);
+var alumno3 = new Alumno("Lucas", 3, true);
 var alumnos = [alumno1, alumno2, alumno3];
-var profesor1 = new Profesores("Alcides", true, alumnos);
-var profesor2 = new Profesores("Evaristo", false, alumnos);
-var profesor3 = new Profesores("Ismael", true, alumnos);
+var profesor1 = new Profesor("Alcides", true, alumnos);
+var profesor2 = new Profesor("Evaristo", false, alumnos);
+var profesor3 = new Profesor("Ismael", true, alumnos);
 var profesores = [profesor1, profesor2, profesor3];
 var escuela1 = new Escuela("N° 39 'Mirador de los Andes'", profesores, alumnos);
 console.log("Escuela:", escuela1.getNombreEscuela());
 console.log("Profesores:");
 for (var i = 0; i < 3; i++) {
-    console.log(profesores[i].getNombreProfesor(), ":", profesores[i].contratarDespedirProfesor());
+    console.log(profesores[i].getNombreProfesor(), ":", escuela1.contratarProfesor(profesores[i]));
 }
 console.log("Alumnos:");
 for (var i = 0; i < 3; i++) {
-    console.log(alumnos[i].getNombreAlumno(), ":", "Nota:", alumnos[i].getNotaExamen(), alumnos[i].getAprobadoDesaprobado(), alumnos[i].matricularAlumno());
+    console.log(alumnos[i].getNombreAlumno(), ":", "Nota:", alumnos[i].getNotaExamen(), alumnos[i].getAprobadoDesaprobado(), escuela1.matricularAlumno(alumnos[i]));
 }
--- index.ts
+++ index.ts
@@ -1,103 +1,105 @@
-class Escuela{
+class Escuela {
     public nombreEscuela: string;
-    public listadoProfesores: Profesores[];
-    public listadoAlumnos: Alumnos[];
+    public listadoProfesores: Profesor[];
+    public listadoAlumnos: Alumno[];
 
-    public constructor(paramNombreEscuela: string, paramListProf: Profesores[], paramListAlum: Alumnos[]){
+    public constructor(paramNombreEscuela: string, paramListProf: Profesor[], paramListAlum: Alumno[]) {
         this.nombreEscuela = paramNombreEscuela;
         this.listadoProfesores = paramListProf;
         this.listadoAlumnos = paramListAlum;
     }
 
-    public getNombreEscuela(): string{
+    public getNombreEscuela(): string {
         return this.nombreEscuela;
+    }
+
+    public contratarProfesor(profesor: Profesor): void {
+        this.listadoProfesores.push(profesor);
+    }
+
+    public despedirProfesor(profesor: Profesor): void {
+        for (let index = 0; index < this.listadoProfesores.length; index++) {
+            if (this.listadoProfesores[index].getNombreProfesor() === profesor.getNombreProfesor()) {
+                this.listadoProfesores.splice(index, 1);
+            }
+        }
+    }
+
+    public matricularAlumno(alumno: Alumno): void {
+        this.listadoAlumnos.push(alumno);
+    }
+
+    public expulsarAlumno(alumno: Profesor): void {
+        for (let index = 0; index < this.listadoAlumnos.length; index++) {
+            if (this.listadoAlumnos[index].getNombreAlumno() === alumno.getNombreProfesor()) {
+                this.listadoAlumnos.splice(index, 1);
+            }
+        }
     }
 }
 
-class Profesores{
+class Profesor {
     public nombreProfesor: string;
     public profesorTieneListado: boolean;
-    public listadoAlumnos: Alumnos[];
+    public listadoAlumnos: Alumno[];
 
-    public constructor(paramNombreProfesor: string,paramProfListYN: boolean, paramListAlum: Alumnos[]){
+    public constructor(paramNombreProfesor: string, paramProfListYN: boolean, paramListAlum: Alumno[]) {
         this.nombreProfesor = paramNombreProfesor;
         this.profesorTieneListado = paramProfListYN
         this.listadoAlumnos = paramListAlum;
     }
 
-    public getNombreProfesor(): string{
+    public getNombreProfesor(): string {
         return this.nombreProfesor;
-    }
-
-    public contratarDespedirProfesor(): string{
-        if(this.profesorTieneListado === true){
-            return "Contratado.";
-        }else{
-            return "Despedido por no tener listado de alumnos.";
-        }
     }
 }
 
-class Alumnos{
+class Alumno {
     public nombreAlumno: string;
     public notaExamen: number;
     public aprobadoDesaprobado: boolean;
-    public informarNota: boolean;
 
-    public constructor(paramNombreAlumno: string, paramNotaExamen: number,paramInformNota: boolean){
+    public constructor(paramNombreAlumno: string, paramNotaExamen: number, paramInformNota: boolean) {
         this.nombreAlumno = paramNombreAlumno;
         this.notaExamen = paramNotaExamen;
-        this.informarNota = paramInformNota;
     }
 
-    public getNombreAlumno(): string{
+    public getNombreAlumno(): string {
         return this.nombreAlumno;
     }
 
-    public getNotaExamen(): number{
+    public getNotaExamen(): number {
         return this.notaExamen;
     }
 
-    public getAprobadoDesaprobado(): string{
-        if(this.notaExamen >= 7){
+    public getAprobadoDesaprobado(): string {
+        if (this.notaExamen >= 7) {
             return "Aprobado";
-        }else{
+        } else {
             return "Desaprobado, a recuperatorio";
-        }
-    }
-
-    public matricularAlumno(): string{
-        while(this.informarNota === true && this.notaExamen >= 7){
-            return "Matriculado.";
-        }if(this.informarNota === false && this.notaExamen >= 7){
-            return "y explusado por no informar su nota.";
-        }else{
-            return "y debe informar su nota en tiempo y forma.";
         }
     }
 }
 
-let alumno1: Alumnos = new Alumnos("Pedro", 9, true);
-let alumno2: Alumnos = new Alumnos("Juan", 7, false);
-let alumno3: Alumnos = new Alumnos("Lucas", 3, true);
-let alumnos: Alumnos[] = [alumno1, alumno2, alumno3];
+let alumno1: Alumno = new Alumno("Pedro", 9, true);
+let alumno2: Alumno = new Alumno("Juan", 7, false);
+let alumno3: Alumno = new Alumno("Lucas", 3, true);
+let alumnos: Alumno[] = [alumno1, alumno2, alumno3];
 
-let profesor1: Profesores = new Profesores("Alcides",true, alumnos);
-let profesor2: Profesores = new Profesores("Evaristo",false, alumnos);
-let profesor3: Profesores = new Profesores("Ismael",true ,alumnos);
-let profesores: Profesores[] = [profesor1, profesor2, profesor3];
+let profesor1: Profesor = new Profesor("Alcides", true, alumnos);
+let profesor2: Profesor = new Profesor("Evaristo", false, alumnos);
+let profesor3: Profesor = new Profesor("Ismael", true, alumnos);
+let profesores: Profesor[] = [profesor1, profesor2, profesor3];
 
 let escuela1: Escuela = new Escuela("N° 39 'Mirador de los Andes'", profesores, alumnos);
 
 console.log("Escuela:", escuela1.getNombreEscuela());
 console.log("Profesores:");
-
-for(let i = 0; i < 3; i++){
-    console.log(profesores[i].getNombreProfesor(), ":", profesores[i].contratarDespedirProfesor());
+for (let i = 0; i < 3; i++) {
+    console.log(profesores[i].getNombreProfesor(), ":", escuela1.contratarProfesor(profesores[i]));
 }
 
 console.log("Alumnos:")
-
-for(let i = 0; i < 3; i++){
-    console.log(alumnos[i].getNombreAlumno(), ":", "Nota:", alumnos[i].getNotaExamen(), alumnos[i].getAprobadoDesaprobado(), alumnos[i].matricularAlumno());
+for (let i = 0; i < 3; i++) {
+    console.log(alumnos[i].getNombreAlumno(), ":", "Nota:", alumnos[i].getNotaExamen(), alumnos[i].getAprobadoDesaprobado(), escuela1.matricularAlumno(alumnos[i]));
 }
